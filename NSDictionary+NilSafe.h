48b92cd85e55f9baf020aaf266c64fa49cb4794e
 孔凡伍 <piaojiangguo@pppppp.local>
2018-06-01 17:26:44 +0800

    add

---
+++ NSDictionary+NilSafe.h
@@ -0,0 +1,17 @@
+//
+//  NSDictionary+NilSafe.h
+//  NSDictionary-NilSafe
+//
+//  Created by Allen Hsu on 6/22/16.
+//  Copyright © 2016 Glow Inc. All rights reserved.
+// 安全创建字典
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (NilSafe)
+
+@end
+
+@interface NSMutableDictionary (NilSafe)
+
+@end
